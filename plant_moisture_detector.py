3231584a0ccf5867d4f9498c26490c7757b550e4
MagdiAref <90858713+MagdiAref@users.noreply.github.com>
2023-07-23 12:55:07 -0400

    Update plant_moisture_detector.py

--- plant_moisture_detector.py
+++ plant_moisture_detector.py
@@ -41,14 +41,14 @@ except KeyboardInterrupt:
 
 #In this code, we use the RPi.GPIO library to interface with the Raspberry Pi's GPIO pins. We set the GPIO mode to BCM and configure the moisture sensor pin (GPIO 17) as an input pin.
 
-We initialize the Pygame mixer to play the sound file. Make sure you have installed the Pygame library (pip install pygame) and replace 'sound.wav' with the actual file path of the sound you want to play.
+#We initialize the Pygame mixer to play the sound file. Make sure you have installed the Pygame library (pip install pygame) and replace 'sound.wav' with the actual file path of the sound you want to play.
 
-The play_sound() function loads the sound file, plays it, waits for the specified duration, and then stops the playback.
+#The play_sound() function loads the sound file, plays it, waits for the specified duration, and then stops the playback.
     
-Inside the main loop, we continuously read the moisture level from the GPIO pin. If the moisture level drops below the threshold, we call the play_sound() function to play the sound.
+#Inside the main loop, we continuously read the moisture level from the GPIO pin. If the moisture level drops below the threshold, we call the play_sound() function to play the sound.
 
-The program runs until it is interrupted by a keyboard interrupt (Ctrl+C), at which point we clean up the GPIO settings.
+#The program runs until it is interrupted by a keyboard interrupt (Ctrl+C), at which point we clean up the GPIO settings.
 
-Remember to adjust the variables (moisture_sensor_pin, moisture_threshold, sound_file, and sound_duration) according to your specific hardware setup and requirements.
+#Remember to adjust the variables (moisture_sensor_pin, moisture_threshold, sound_file, and sound_duration) according to your specific hardware setup and requirements.
 
-Note: This example assumes you have connected the moisture sensor's signal pin to GPIO 17. Adjust the moisture_sensor_pin variable accordingly if you use a different GPIO pin. Additionally, ensure that the sound file you provide is compatible with the Pygame mixer library for playback.
+#Note: This example assumes you have connected the moisture sensor's signal pin to GPIO 17. Adjust the moisture_sensor_pin variable accordingly if you use a different GPIO pin. Additionally, ensure that the sound file you provide is compatible with the Pygame mixer library for playback. 
